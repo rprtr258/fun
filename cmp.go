2f098f6c6566c5150593ab636d37963a299c184c
Poletanskiy Viktor <vpoletanskiy@ozon.ru>
2023-08-26 22:01:24 +0300

    [master] add MinBy, MaxBy

--- cmp.go
+++ cmp.go
@@ -29,3 +29,33 @@ func Max[T cmp.Ordered](xs ...T) T {
 func Clamp[T cmp.Ordered](x, low, high T) T {
 	return max(low, min(x, high))
 }
+
+func MinBy[T any, R cmp.Ordered](f func(T) R, xs ...T) T {
+	if len(xs) == 0 {
+		return Zero[T]()
+	}
+
+	res := xs[0]
+	fres := f(res)
+	for _, x := range xs[1:] {
+		if fx := f(x); fres > fx {
+			res, fres = x, fx
+		}
+	}
+	return res
+}
+
+func MaxBy[T any, R cmp.Ordered](f func(T) R, xs ...T) T {
+	if len(xs) == 0 {
+		return Zero[T]()
+	}
+
+	res := xs[0]
+	fres := f(res)
+	for _, x := range xs[1:] {
+		if fx := f(x); fres < fx {
+			res, fres = x, fx
+		}
+	}
+	return res
+}
